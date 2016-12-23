cd5f5e6754cc7601fb2aac632dfe7e3b673cb165
jmcota <jmcota@ontica.org>
2016-12-22 23:47:00 -0500

    web.config setup to use HTML5 client routes under IIS servers
    IIS URL Rewrite module must be installed. Please see:
    https://www.iis.net/downloads/microsoft/url-rewrite

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -95,7 +95,7 @@ export class SeedConfig {
    * which can be overriden by the `--base` flag when running `npm start`.
    * @type {string}
    */
-  APP_BASE = argv['base'] || '/';
+  APP_BASE = argv['base'] || '/consultas/';
 
   /**
    * The base path of node modules.
