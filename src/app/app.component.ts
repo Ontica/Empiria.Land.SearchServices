7b399c19d0a70e6c9b5aafe1ff40c11d048bbce5
jmcota <jmcota@ontica.org>
2019-10-12 12:12:59 -0500

    Rfx Reorganize image assets (icons and favicons)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,38 +6,39 @@
  */
 
 import { Component } from '@angular/core';
-import { APP_SETTINGS } from './empiria.config';
+import { APP_SETTINGS } from '../assets/empiria.config';
+
 
 /**
  * This class represents the main application component.
  */
 @Component({
-  selector: 'app-root',
+  selector: 'emp-ng-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
 
-  public get mainLogo() {
+  get mainLogo() {
     if (APP_SETTINGS.displayVedaElectoralUI) {
-      return './assets/images/customer/main.logo.veda.png';
+      return './assets/img/customer/main.logo.veda.png';
     }
-    return './assets/images/customer/main.logo.png';
+    return './assets/img/customer/main.logo.png';
   }
 
 
-  public get secondaryLogo() {
+  get secondaryLogo() {
     if (APP_SETTINGS.displayVedaElectoralUI) {
-      return './assets/images/customer/secondary.logo.veda.png';
+      return './assets/img/customer/secondary.logo.veda.png';
     }
-    return './assets/images/customer/secondary.logo.png';
+    return './assets/img/customer/secondary.logo.png';
   }
 
 
-  public get advertisementImage() {
+  get advertisementImage() {
     if (APP_SETTINGS.displayVedaElectoralUI) {
-      return './assets/images/customer/advertisement.veda.jpg';
+      return './assets/img/customer/advertisement.veda.jpg';
     }
-    return './assets/images/customer/advertisement.jpg';
+    return './assets/img/customer/advertisement.jpg';
   }
 
 }
